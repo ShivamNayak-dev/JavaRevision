da7ce25fbbea8272a1a2128ce84b505f8baebd64
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-04-06 00:44:37 +0530

    Default constructor example

---
+++ Oops Concepts/DefaultConstructors.java
@@ -0,0 +1,13 @@
+class Laptop{
+    String Brand;
+    int Ram;
+}
+
+public class DefaultConstructors{
+    public static void main(String[] args){
+        Laptop l = new Laptop();
+
+        System.out.println(l.Brand);
+        System.out.println(l.Ram);
+    }
+}
